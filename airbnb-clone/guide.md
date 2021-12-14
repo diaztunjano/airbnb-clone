8bd5a5f6fdb631ef37bdd396cdb4fe7860a9b1d3
diaztunjano <dfdiaz1@uc.cl>
2021-12-14 15:25:50 -0300

    feat(explore-nearby): fetching data from JSON

--- airbnb-clone/guide.md
+++ airbnb-clone/guide.md
@@ -19,3 +19,4 @@ This document serves as guidance to the process of creating this app. It uses Ne
 
 ### NextJS Consideration
 - IMG Tag: Instead of using normal html `<img src="image.jpg"/>`, we use Next.js `import Image from "next/image"`; because it optimizes the image into webp format which is smaller and faster to load. We need to create *next.config.js* file and write the source from where the image is being loaded. 
+- Static Rendering: When including `async function getStaticProps()`, it tells Next.js to do server rendering. Normally, User get the React Bundle when entering the website. Next.js introduces a server between the user and React. First, the user makes a request to the Next.js server and it sends only the components that the website needs. With Server Side Rendering, everytime the user requests a site, it reloads from zero but it is still faster than regular React bundle.  
--- airbnb-clone/pages/index.js
+++ airbnb-clone/pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+const axios = require("axios");
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <div>
       <Head>
@@ -10,14 +11,42 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
       {/* Header */}
-      <Header/>
+      <Header />
 
-      
       {/* Banner */}
-      <Banner/>
+      <Banner />
 
+      {/* Main */}
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6 ">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          {/* Data from API Endpoint */}
+
+          {data?.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
   );
+}
+
+// This function runs before it reaches the browser and it passes the props to the functional component Home as props
+export async function getStaticProps() {
+  const options = {
+    method: "GET",
+    url: "https://jsonkeeper.com/b/4G1G",
+  };
+
+  const { data } = await axios(options);
+
+  console.log(data);
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
